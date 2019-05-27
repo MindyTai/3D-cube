8b388ad1217fb6286f165bd3e41aa3ff024545e2
mindytai175 <mindytai175@gmail.com>
2019-05-27 15:18:54 +0800

    WIP

--- js/main.js
+++ js/main.js
@@ -1,19 +1,60 @@
 'use strict'
-var startY = null;
+var startX = null;
 var flag = 0;
+var TURN_LEFT = 0;
+var TURN_RIGHT = 1;
+var swipeDirection = TURN_LEFT;
+var currentIndex;
+var count = 0;
+
+var counter = function(){
+  count = count + 1;
+  console.log(count);
+}
+
+var detect = setInterval(counter, 1000);
+
+function stop(){
+  clearInterval(detect);
+  console.log("now is" + count);
+}
 
 function Cube(cube){
   this.cubeDom = cube;
 }
 
 Cube.prototype = {
-  touchStop: function(dom){
+  touchStop: function(dom, e){
     dom.classList.add("rotateStop");
-    dom.classList.remove("rotateStart");
+    
+    if(count%6 === 1){
+      dom.classList.add('rotate','rotate-2');
+    }else if(count%6 === 2){
+      dom.classList.add('rotate','rotate-3');
+    }else if(count%6 === 3){
+      dom.classList.add('rotate', 'rotate-4');
+    }else if(count%6 === 4){
+      dom.classList.add('rotate','rotate-5');
+    }else if(count%6 === 5){
+      dom.classList.add('rotate','rotate-6');
+    }else if(count%6 === 0){
+      dom.classList.add('rotate','rotate-1');
+    }
+    // console.log(dom);
+    // console.log(dom.hasAttribute('width'));
+    // console.log(e.target.id);
+    
+    // dom.classList.add("space-rotate");
+    // dom.classList.add("rotate-2");
+    // setTimeout(() => dom.classList.add("rotate-3"), 500);
+
+    // dom.classList.add("rotate-2");
+    // dom.classList.remove("rotateStart");
+    
   },
   touchStart: function(dom){
-    dom.classList.add("rotateStart");
-    dom.classList.remove("rotateStop");
+    // dom.classList.add("rotateStart");  
+    // dom.classList.remove("rotateStop");
   }
 }
 
@@ -40,15 +81,42 @@ function _touchStartHandler(e) {
   var cube = document.getElementsByClassName("space")[0];
   var Cubie = new Cube(cube);
   var touchobj = e.changedTouches[0];
-  startY       = touchobj.pageY;
+  startX      = touchobj.pageX;
 
   if(flag === 0){
-    Cubie.touchStop(cube);
+    Cubie.touchStop(cube, e);
+    stop();
+   
     flag = 1;
   } else {
     Cubie.touchStart(cube);
     flag = 0;
   }
+}
+
+function _touchMoveHandler(e) {
+  e.stopPropagation();
+if (e.cancelable) {
+  e.preventDefault();
+} else
+  return false;
+}
+
+function _touchEndHandler(e) {
+  var touchobj = e.changedTouches[0];
+  if ( startX !== null && startX !== touchobj.pageX ) {
+    swipeDirection = touchobj.pageX < startX ? TURN_LEFT : TURN_RIGHT;
+    currentIndex = swipeDirection === TURN_LEFT ? _nextIndex(currentIndex, -1) : _nextIndex(currentIndex, 1);
+    startX = null;
+  }
+}
+
+//next box
+function _nextIndex(currentIndex, number) {
+  var GroupCnt = document.getElementsByClassName("box").length;
+  var remainder = (currentIndex - number) % GroupCnt;
+
+  return remainder < 0 ? remainder + GroupCnt : remainder;
 }
 
 function _init(){
@@ -59,11 +127,14 @@ function _init(){
   var Startbutton = new Buttons(start_button);
   var StopButton = new Buttons(stop_button);
  
-
   Startbutton.pressStart(cube);
   StopButton.pressStop(cube);
-  
+ 
+
   cube.addEventListener('touchstart', _touchStartHandler);
+  cube.addEventListener('touchmove', function(e){ e.preventDefault() });
+  cube.addEventListener('touchend', _touchEndHandler);
 }
 
 _init();
+
