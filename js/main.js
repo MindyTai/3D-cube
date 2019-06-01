ee2620d960dc11370d90c2ffab65d9c1e2989bd4
mindytai175 <mindytai175@gmail.com>
2019-06-02 02:09:12 +0800

    WIP

--- js/main.js
+++ js/main.js
@@ -1,142 +1,158 @@
-var startX = null;
-var startY = null;
-var TURN_LEFT = 0;
-var TURN_RIGHT = 1;
-var swipeDirection = TURN_LEFT;
+var DIRECTION = {
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+  UP: 'UP',
+  DOWN: 'DOWN'
+};
 
-var keyframeA = 'rotateX(0deg) rotateY(0deg) rotateZ(0deg)';
-var keyframeB = 'rotateX(-90deg) rotateY(0deg) rotateZ(0deg)';
-var keyframeC = 'rotateX(-90deg) rotateY(0deg) rotateZ(90deg)';
-var keyframeD = 'rotateX(-180deg) rotateY(0deg) rotateZ(90deg)';
-var keyframeE = 'rotateX(-180deg) rotateY(-90deg) rotateZ(90deg)';
-var keyframeF = 'rotateX(-180deg) rotateY(-90deg) rotateZ(180deg)';
+var SIDE = {
+  A: 'A',
+  B: 'B',
+  C: 'C',
+  D: 'D',
+  E: 'E',
+  F: 'F'
+};
 
-var cubeDom = document.getElementsByClassName('space')[0];
+var STATE = {
+  INIT: 'INIT',
+  ACTIVE: 'ACTIVE'
+};
 
-// function firstRotation() {
-//   cube.rotateA(cubeDom);
-// }
+var startX;
+var startY;
+var swipeDirection;
 
 function Cube(dom) {
-  this.cubeDom = dom;
+  this.dom = dom;
+  this.side = SIDE.A;
+  this.state = STATE.INIT;
+  this.detect = this.detect.bind(this);
+  this.dom.addEventListener('transitionend', this.detect);
 }
 
 Cube.prototype = {
-  turnRight(dom, e) {
-    console.log(document.styleSheets[0].cssRules[2].style.animation);
-    var el = document.getElementsByClassName('space')[0];
-    var st = getComputedStyle(el);
-    var tr =
-      st.getPropertyValue('-webkit-transform') ||
-      st.getPropertyValue('-moz-transform') ||
-      st.getPropertyValue('-ms-transform') ||
-      st.getPropertyValue('-o-transform') ||
-      st.getPropertyValue('transform') ||
-      "Either no transform set, or browser doesn't do getComputedStyle";
-
-    console.log(tr);
-    console.log(e.target.id);
-    var { id } = e.target;
-    var keyframe;
-
-    if (id === '1') {
-      keyframe = keyframeF;
-    } else if (id === '2') {
-      keyframe = keyframeA;
-    } else if (id === '3') {
-      keyframe = keyframeB;
-    } else if (id === '4') {
-      keyframe = 'rotateX(-90deg) rotateY(0deg) rotateZ(-270deg)';
-    } else if (id === '5') {
-      keyframe = 'rotateX(-180deg) rotateY(0deg) rotateZ(-270deg)';
-    } else if (id === '6') {
-      keyframe = keyframeA;
+  detect() {
+    if (this.dom.classList.contains('rotate-b')) {
+      this.rotateBtoC(this.dom);
+    } else if (this.dom.classList.contains('rotate-c')) {
+      this.rotateCtoD(this.dom);
+    } else if (this.dom.classList.contains('rotate-d')) {
+      this.rotateDtoE(this.dom);
+    } else if (this.dom.classList.contains('rotate-e')) {
+      this.rotateEtoF(this.dom);
+    } else if (this.dom.classList.contains('rotate-f')) {
+      this.rotateFtoX(this.dom);
+    } else if (this.dom.classList.contains('rotate-a-1')) {
+      this.rotateXtoA(this.dom);
+      this.rotateAtoB(this.dom);
     }
-
-    var root = document.documentElement.style;
-    root.setProperty('--angle', tr);
-    root.setProperty('--final', keyframe);
-
-    dom.classList.add('space-rotate');
   },
-  turnLeft(dom, e) {
-    var el = document.getElementsByClassName('space')[0];
-    var st = getComputedStyle(el);
-    var tr =
-      st.getPropertyValue('-webkit-transform') ||
-      st.getPropertyValue('-moz-transform') ||
-      st.getPropertyValue('-ms-transform') ||
-      st.getPropertyValue('-o-transform') ||
-      st.getPropertyValue('transform') ||
-      "Either no transform set, or browser doesn't do getComputedStyle";
-
-    console.log(tr);
-    var { id } = e.target;
-    var keyframe;
-
-    if (id === '1') {
-      keyframe = keyframeB;
-    } else if (id === '2') {
-      keyframe = keyframeC;
-    } else if (id === '3') {
-      keyframe = keyframeD;
-    } else if (id === '4') {
-      keyframe = keyframeE;
-    } else if (id === '5') {
-      keyframe = keyframeF;
-    } else if (id === '6') {
-      keyframe = keyframeA;
-    }
-
-    var root = document.documentElement.style;
-    root.setProperty('--angle', tr);
-    root.setProperty('--final', keyframe);
-
-    dom.classList.add('space-rotate');
+  rotate(from, to, doTrans) {},
+  rotateAtoB() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-a');
+    this.dom.classList.add('rotate-b');
+    this.side = 'A';
   },
-  rotateA(dom) {
-    var x = dom.clientHeight;
-    dom.classList.remove('rotate-a');
-    dom.classList.add('rotate-b');
+  rotateBtoA() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-b');
+    this.dom.classList.add('rotate-a');
+    this.side = 'B';
   },
-  firstRotation(dom) {
-    this.rotateA(dom);
+  rotateBtoC() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-b');
+    this.dom.classList.add('rotate-c');
+    this.side = 'B';
   },
-  rotateB(dom) {
-    var x = dom.clientHeight;
-    dom.classList.remove('rotate-b');
-    dom.classList.add('rotate-c');
+  rotateCtoB() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-c');
+    this.dom.classList.add('rotate-b');
+    this.side = 'C';
   },
-  rotateC(dom) {
-    var x = dom.clientHeight;
-    dom.classList.remove('rotate-c');
-    dom.classList.add('rotate-d');
+  rotateCtoD() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-c');
+    this.dom.classList.add('rotate-d');
+    this.side = 'C';
   },
-  rotateD(dom) {
-    var x = dom.clientHeight;
-    dom.classList.remove('rotate-d');
-    dom.classList.add('rotate-e');
+  rotateDtoC() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-d');
+    this.dom.classList.add('rotate-c');
+    this.side = 'D';
   },
-  rotateE(dom) {
-    var x = dom.clientHeight;
-    dom.classList.remove('rotate-e');
-    dom.classList.add('rotate-f');
+  rotateDtoE() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-d');
+    this.dom.classList.add('rotate-e');
+    this.side = 'D';
   },
-  rotateF(dom) {
-    var x = dom.clientHeight;
-    dom.classList.remove('rotate-f');
-    dom.classList.add('rotate-a-1');
+  rotateEtoD() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-e');
+    this.dom.classList.add('rotate-d');
+    this.side = 'E';
   },
-  rotateX(dom) {
-    dom.classList.remove('rotate-a-1');
-    dom.classList.add('rotate-a');
-    var x = dom.clientHeight;
-    this.firstRotation(dom);
+  rotateEtoF() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-e');
+    this.dom.classList.add('rotate-f');
+    this.side = 'E';
   },
+  rotateFtoE() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-f');
+    this.dom.classList.add('rotate-e');
+    this.side = 'F';
+  },
+  rotateFtoX() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-f');
+    this.dom.classList.add('rotate-a-1');
+    this.side = 'F';
+  },
+  rotateXtoF() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-a-1');
+    this.dom.classList.add('rotate-f');
+    this.side = 'X';
+  },
+  rotateXtoA() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.remove('transition');
+    this.dom.classList.remove('rotate-a-1');
+    this.dom.classList.add('rotate-a');
+    this.side = 'X';
+  },
+  rotateAtoX() {
+    var x = this.dom.clientHeight;
+    this.dom.classList.add('transition');
+    this.dom.classList.remove('rotate-reverse-a');
+    this.dom.classList.add('rotate-reverse-a-1');
+    this.side = 'A';
+  }
 };
 
+var cubeDom = document.getElementsByClassName('space')[0];
 var cube = new Cube(cubeDom);
-cube.firstRotation(cubeDom);
+cube.rotateAtoB();
+
 // touch
 function _touchStartHandler(e) {
   var touchobj = e.changedTouches[0];
@@ -152,40 +168,151 @@ function _touchMoveHandler(e) {
 }
 
 function _touchEndHandler(e) {
-  console.log(startY);
   var touchobj = e.changedTouches[0];
-  if (startX !== null && startX !== touchobj.pageX) {
-    swipeDirection = touchobj.pageX > startX ? TURN_RIGHT : TURN_LEFT;
-    if (swipeDirection === TURN_LEFT) {
-      cube.turnLeft(cubeDom, e);
-    } else {
-      cube.turnRight(cubeDom, e);
+
+  if (
+    startX != null &&
+    startX !== touchobj.pageX &&
+    startY != null &&
+    startY !== touchobj.pageY
+  ) {
+    // swipe down
+    if (
+      Math.abs(touchobj.pageY - startY) > Math.abs(touchobj.pageX - startX) &&
+      touchobj.pageY > startY
+    ) {
+      swipeDirection = DIRECTION.DOWN;
+    } else if (
+      Math.abs(touchobj.pageY - startY) > Math.abs(touchobj.pageX - startX) &&
+      touchobj.pageY < startY
+    ) {
+      swipeDirection = DIRECTION.UP;
+    } else if (
+      Math.abs(touchobj.pageY - startY) < Math.abs(touchobj.pageX - startX) &&
+      touchobj.pageX > startX
+    ) {
+      swipeDirection = DIRECTION.RIGHT;
+    } else if (
+      Math.abs(touchobj.pageY - startY) < Math.abs(touchobj.pageX - startX) &&
+      touchobj.pageX < startX
+    ) {
+      swipeDirection = DIRECTION.LEFT;
     }
+
+    // swipe down
+    if (swipeDirection === DIRECTION.DOWN) {
+      if (cube.side === 'A') {
+        cube.rotateBtoA();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'C') {
+        cube.rotateDtoC();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'E') {
+        cube.rotateFtoE();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      }
+    }
+    // swipe up
+    if (swipeDirection === DIRECTION.UP) {
+      if (cube.side === 'A') {
+        cube.rotateAtoB();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'C') {
+        cube.rotateCtoD();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'E') {
+        cube.rotateEtoF();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      }
+    }
+    // swipe right
+    if (swipeDirection === DIRECTION.LEFT) {
+      if (cube.side === 'B') {
+        cube.rotateBtoC();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'D') {
+        cube.rotateDtoE();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'F') {
+        cube.rotateFtoX();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      }
+    }
+    // // side A
+    // if (cube.side === 'A') {
+    //   if (swipeDirection === DIRECTION.DOWN) {
+    //     cube.rotateBtoA();
+    //     cube.dom.removeEventListener('transitionend', cube.detect);
+    //   } else {
+    //     cube.rotateAtoB();
+    //     cube.dom.removeEventListener('transitionend', cube.detect);
+    //   }
+    // }
+
+    // // side B
+    // if (cube.side === 'B') {
+    //   if (swipeDirection === DIRECTION.LEFT) {
+    //     cube.rotateCtoB();
+    //     cube.dom.removeEventListener('transitionend', cube.detect);
+    //   } else {
+    //     cube.rotateBtoC();
+    //     cube.dom.removeEventListener('transitionend', cube.detect);
+    //   }
+    // }
+
+    // // side C
+    // if (cube.side === 'C') {
+    //   cube.rotateDtoC();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // } else {
+    //   cube.rotateCtoD();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // }
+
+    // // side D
+    // if (cube.side === 'D') {
+    //   cube.rotateEtoD();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // } else {
+    //   cube.rotateDtoE();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // }
+
+    // // side E
+    // if (cube.side === 'E') {
+    //   cube.rotateFtoE();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // } else {
+    //   cube.rotateEtoF();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // }
+
+    // // side F
+    // if (cube.side === 'F') {
+    //   cube.rotateXtoF();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // } else {
+    //   cube.rotateFtoX();
+    //   cube.dom.removeEventListener('transitionend', cube.detect);
+    // }
+
+    // swipe left
+    if (swipeDirection === DIRECTION.RIGHT) {
+      if (cube.side === 'B') {
+        cube.rotateCtoB();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'D') {
+        cube.rotateEtoD();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      } else if (cube.side === 'F') {
+        cube.rotateXtoF();
+        cube.dom.removeEventListener('transitionend', cube.detect);
+      }
+    }
+
     startX = null;
   }
-  // else if( startY !== null && startY !== touchobj.pageY ){
-  //   swipeDirection = touchobj.pageY > startY ? TURN_RIGHT : TURN_RIGHT;
-  //   swipeDirection == TURN_RIGHT ? cube.turnRight(cube,e) : cube.turnRight(cube,e);
-  //   startY = null;
-  // }
 }
-
-cubeDom.addEventListener('transitionend', function() {
-  console.log(cubeDom.classList);
-  if (cubeDom.classList.contains('rotate-b')) {
-    cube.rotateB(cubeDom);
-  } else if (cubeDom.classList.contains('rotate-c')) {
-    cube.rotateC(cubeDom);
-  } else if (cubeDom.classList.contains('rotate-d')) {
-    cube.rotateD(cubeDom);
-  } else if (cubeDom.classList.contains('rotate-e')) {
-    cube.rotateE(cubeDom);
-  } else if (cubeDom.classList.contains('rotate-f')) {
-    cube.rotateF(cubeDom);
-  } else if (cubeDom.classList.contains('rotate-a-1')) {
-    cube.rotateX(cubeDom);
-  }
-});
 
 function _init() {
   cubeDom.addEventListener('touchstart', _touchStartHandler);
